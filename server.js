1a477c9455cfcd341dc558e50c4e11289da192b9
Naresh Mongroo <nmongroo@hotmail.com>
2016-01-02 14:59:26 -0400

    Proj init

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+
+// app.get('/', function (req, res){
+// 	res.send('hello express');
+// });
+
+app.get('/about', function (req, res){
+	res.send('About page');
+});
+
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(3000, function () {
+	console.log('Express server started on port 3000 !');
+});
